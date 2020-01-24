9e9724679514d23aca5bb157d5b79b99daeef78c
Lukas Bombach <lukas.bombach@googlemail.com>
2020-01-24 15:05:36 +0100

    added storybook designs addon

--- packages/storybook/stories/Button.stories.tsx
+++ packages/storybook/stories/Button.stories.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { withDesign } from "storybook-addon-designs";
 import { Button } from "@storybook/react/demo";
 
-export default { title: "Button" };
+export default { title: "Button", component: Button, decorators: [withDesign] };
 
 export const withText = () => <Button>Hello Button</Button>;
+
+withText.story = {
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/NfPcczxt0Verbv5NNvulS8",
+    },
+  },
+};
 
 export const withEmoji = () => (
   <Button>
@@ -12,3 +22,12 @@ export const withEmoji = () => (
     </span>
   </Button>
 );
+
+withEmoji.story = {
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/NfPcczxt0Verbv5NNvulS8",
+    },
+  },
+};
