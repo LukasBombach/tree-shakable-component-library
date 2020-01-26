fa82e3e07fa5df0ea6c5ae004d5143930c71df3e
Lukas Bombach <lukas.bombach@googlemail.com>
2020-01-26 22:24:57 +0100

    fixed build system, imported components

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,3 +1,16 @@
+import Button from "ui-library/lib/Button/Button";
+import Link from "ui-library/lib/Link/Link";
+
 export default function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div>
+      <h1>Welcome to t-online</h1>
+      <p>
+        <Button>I'm a button</Button>
+      </p>
+      <p>
+        <Link>I'm a link</Link>
+      </p>
+    </div>
+  );
 }
